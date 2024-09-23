5a4eab5b8c33aa2719f3da8b3aefd49d4c5d2589
lijames2 <li.james.216@gmail.com>
2024-09-23 16:53:32 -0400

    Initial Commit

---
+++ ScoreSwipe Watch App/ScoreSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreSwipeApp.swift
+//  ScoreSwipe Watch App
+//
+//  Created by James Li on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoreSwipe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScoreSwipe/ScoreSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreSwipeApp.swift
+//  ScoreSwipe
+//
+//  Created by James Li on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoreSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
