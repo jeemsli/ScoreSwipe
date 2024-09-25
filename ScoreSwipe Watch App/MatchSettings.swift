2b7ebdc63480b43876c692fc87cfa902be8d6850
lijames2 <li.james.216@gmail.com>
2024-09-25 14:46:04 -0400

    Modify MatchSettings and MatchSettingsView

--- ScoreSwipe Watch App/MatchSettings.swift
+++ ScoreSwipe Watch App/MatchSettings.swift
@@ -1,29 +1,18 @@
 import Foundation
 
-// Who serves first?
-enum ServingTeam: String, CaseIterable {
-    case ourTeam = "Our Team"
-    case opponentTeam = "Opponent Team"
-}
-
-// Which side the player starts?
-enum StartingSide: String, CaseIterable {
-    case left = "Left"
-    case right = "Right"
-}
-
-class MatchSettings: ObservableObject {
-    @Published var isDoubles: Bool = true
-    @Published var matchPoint: Int = 11
-    @Published var servingTeam: ServingTeam = .ourTeam
-    @Published var startingSide: StartingSide = .right
+@Observable
+class MatchSettings {
+    var isDoubles: Bool = true
+    var matchPoint: Int = 11
+    var servingTeam: String = "Us"
+    var startingSide: String = "Left"
     
     let matchPoints = [11, 15, 21]
-
+    
     func reset() {
         isDoubles = true
         matchPoint = 11
-        servingTeam = .ourTeam
-        startingSide = .right
+        servingTeam = "Us"
+        startingSide = "Left"
     }
 }
--- ScoreSwipe Watch App/MatchSettingsView.swift
+++ ScoreSwipe Watch App/MatchSettingsView.swift
@@ -1,145 +1,85 @@
 import SwiftUI
 
-//struct MatchSettingsView: View {
-//    @ObservedObject var matchSettings: MatchSettings
-//
-//    let columns = [
-//        GridItem(.flexible()),
-//        GridItem(.flexible())
-//    ]
-//
-//    var body: some View {
-//        ScrollView {
-//            LazyVGrid(columns: columns) {
-//                // Mode Picker
-//                VStack {
-//                    Text("Mode")
-//                    Picker("Mode", selection: $matchSettings.isDoubles) {
-//                        Text("Doubles")
-//                    }
-//                    .labelsHidden()
-//                    .frame(minWidth: 50, minHeight: 35)
-//                }
-//
-//                // Match Point Selector
-//                VStack {
-//                    Text("Point")
-//                    Picker("Point", selection: $matchSettings.matchPoint) {
-//                        ForEach(matchSettings.matchPoints, id: \.self) { points in
-//                            Text("\(points)")
-//                        }
-//                    }
-//                    .labelsHidden()
-//                    .frame(minWidth: 50, minHeight: 35)
-//                }
-//
-//                // First Serve Picker
-//                VStack {
-//                    Text("Serving")
-//                    Picker("Serving", selection: $matchSettings.servingTeam) {
-//                        Text("Us")
-//                        Text("Them")
-//                    }
-//                    .labelsHidden()
-//                    .frame(minWidth: 50, minHeight: 35)
-//                }
-//
-//                // Side Picker
-//                VStack {
-//                    Text("Side")
-//                    Picker("Side", selection: $matchSettings.startingSide) {
-//                        Text("Right")
-//                        Text("Left")
-//                    }
-//                    .labelsHidden()
-//                    .frame(minWidth: 50, minHeight: 35)
-//                }
-//            }
-//            .padding(.bottom)
-//
-//            Button("Go") {
-//
-//            }
-//            .foregroundStyle(.green)
-//        }
-//    }
-//}
-
 struct MatchSettingsView: View {
-    @ObservedObject var matchSettings: MatchSettings
+    var matchSettings: MatchSettings
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack {
-                    Text("Mode")
-                        .font(.headline)
-                    HStack {
-                        Button("Doubles") {
-                            matchSettings.isDoubles = true
-                        }
-                        .background(matchSettings.isDoubles ? Color.blue : Color.black)
-                        .clipShape(.capsule)
-                        .animation(.snappy, value: matchSettings.servingTeam)
-                    }
-                    
-                    Text("Match Point")
-                        .font(.headline)
-                    HStack {
-                        ForEach(matchSettings.matchPoints, id: \.self) { points in
-                            Button("\(points)") {
-                                matchSettings.matchPoint = points
+            ScrollViewReader { scroll in
+                ScrollView {
+                    VStack {
+                        Text("Mode")
+                            .font(.headline)
+                            .id("top")
+                        HStack {
+                            Button("Doubles") {
+                                matchSettings.isDoubles = true
                             }
-                            .background(matchSettings.matchPoint == points ? Color.blue : Color.black)
+                            .background(matchSettings.isDoubles ? Color.blue : Color.black)
                             .clipShape(.capsule)
-                            .opacity(matchSettings.matchPoint == points ? 1 : 0.5)
+                            .animation(.snappy, value: matchSettings.servingTeam)
                         }
-                    }
-                    
-                    Text("First Serve")
-                        .font(.headline)
-                    HStack {
-                        Button("Us") {
-                            matchSettings.servingTeam = .ourTeam
-                        }
-                        .background(matchSettings.servingTeam == .ourTeam ? Color.blue : Color.black)
-                        .clipShape(.capsule)
-                        .opacity(matchSettings.servingTeam == .ourTeam ? 1 : 0.5)
                         
-                        Button("Them") {
-                            matchSettings.servingTeam = .opponentTeam
+                        Text("Match Point")
+                            .font(.headline)
+                        HStack {
+                            ForEach(matchSettings.matchPoints, id: \.self) { points in
+                                Button("\(points)") {
+                                    matchSettings.matchPoint = points
+                                }
+                                .background(matchSettings.matchPoint == points ? Color.blue : Color.black)
+                                .clipShape(.capsule)
+                                .opacity(matchSettings.matchPoint == points ? 1 : 0.5)
+                            }
                         }
-                        .background(matchSettings.servingTeam == .opponentTeam ? Color.blue : Color.black)
-                        .clipShape(.capsule)
-                        .opacity(matchSettings.servingTeam == .opponentTeam ? 1 : 0.5)
-                    }
-                    
-                    Text("Starting Side")
-                        .font(.headline)
-                    HStack {
-                        Button("Left") {
-                            matchSettings.startingSide = .left
-                        }
-                        .background(matchSettings.startingSide == .left ? Color.blue : Color.black)
-                        .clipShape(.capsule)
-                        .opacity(matchSettings.startingSide == .left ? 1 : 0.5)
                         
-                        Button("Right") {
-                            matchSettings.startingSide = .right
+                        Text("First Serve")
+                            .font(.headline)
+                        HStack {
+                            Button("Us") {
+                                matchSettings.servingTeam = "Us"
+                            }
+                            .background(matchSettings.servingTeam == "Us" ? Color.blue : Color.black)
+                            .clipShape(.capsule)
+                            .opacity(matchSettings.servingTeam == "Us" ? 1 : 0.5)
+                            
+                            Button("Them") {
+                                matchSettings.servingTeam = "Them"
+                            }
+                            .background(matchSettings.servingTeam == "Them" ? Color.blue : Color.black)
+                            .clipShape(.capsule)
+                            .opacity(matchSettings.servingTeam == "Them" ? 1 : 0.5)
                         }
-                        .background(matchSettings.startingSide == .right ? Color.blue : Color.black)
-                        .clipShape(.capsule)
-                        .opacity(matchSettings.startingSide == .right ? 1 : 0.5)
-                    }
-                    .padding(.bottom)
-                    
-                    Button("Go") {
                         
+                        Text("Starting Side")
+                            .font(.headline)
+                        HStack {
+                            Button("Left") {
+                                matchSettings.startingSide = "Left"
+                            }
+                            .background(matchSettings.startingSide == "Left" ? Color.blue : Color.black)
+                            .clipShape(.capsule)
+                            .opacity(matchSettings.startingSide == "Left" ? 1 : 0.5)
+                            
+                            Button("Right") {
+                                matchSettings.startingSide = "Right"
+                            }
+                            .background(matchSettings.startingSide == "Right" ? Color.blue : Color.black)
+                            .clipShape(.capsule)
+                            .opacity(matchSettings.startingSide == "Right" ? 1 : 0.5)
+                        }
+                        .padding(.bottom)
+                        
+                        NavigationLink(destination: MatchView(matchSettings: matchSettings)) {
+                            Text("Start Match")
+                                .foregroundStyle(.green)
+                        }
                     }
-                    .foregroundStyle(.green)
+                }
+                .onAppear {
+                    scroll.scrollTo("top", anchor: .top)
                 }
             }
-            
         }
     }
 }
