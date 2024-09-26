2500893b770775620d25986498321b86f842a768
lijames2 <li.james.216@gmail.com>
2024-09-26 17:34:25 -0400

    Create scoreView

---
+++ ScoreSwipe Watch App/ScoreView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+struct ScoreView: View {
+    var match: Match
+    
+    init(match: Match) {
+        self.match = match
+    }
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            if match.servingTeam == "Us" {
+                scoreText(match.ourScore, color: .blue)
+                scoreText(match.opponentScore, color: .red)
+            } else {
+                scoreText(match.opponentScore, color: .red)
+                scoreText(match.ourScore, color: .blue)
+            }
+            scoreText(match.position, color: .white)
+            Spacer()
+        }
+    }
+    
+    private func scoreText(_ score: Int, color: Color) -> some View {
+        Text("\(score)")
+            .font(.largeTitle)
+            .bold()
+            .foregroundColor(color)
+            .frame(maxWidth: .infinity)
+            .multilineTextAlignment(.center)
+    }
+}
+
+#Preview {
+    var matchSettings = MatchSettings()
+    var undoManager = UndoManager()
+    var match = Match(settings: matchSettings, undoManager: undoManager)
+    
+    return ScoreView(match: match)
+}
