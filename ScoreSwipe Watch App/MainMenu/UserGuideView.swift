f9ff7a928f508c54af4bb1ed1a5bb7fae8412c34
lijames2 <li.james.216@gmail.com>
2024-09-29 23:50:05 -0400

    Update user guide

--- ScoreSwipe Watch App/MainMenu/UserGuideView.swift
+++ ScoreSwipe Watch App/MainMenu/UserGuideView.swift
@@ -13,18 +13,26 @@ struct UserGuideView: View {
                 guideItem(image: "arrow.down", color: .blue, text: "Swipe Down: Your team wins the rally.")
                 Divider()
                 guideItem(image: "arrow.left", color: .white, text: "Swipe Left: Undo the rally.")
-                Text("Note: For accurate results, swipe from the middle of the screen.")
-                    .font(.headline)
+                Text("For accurate results, swipe from the middle of the screen.")
+                    .italic()
             }
         }
     }
     
     private var legendSection: some View {
         VStack(alignment: .leading, spacing: 10) {
-            legendItem(color: .blue, text: "Your team's score is blue.")
-            legendItem(color: .red, text: "Opponent team's score is red.")
-            Text("Highlighted player will be serving.")
+            legendItem(color: .blue, text: "Your team consists of you and your partner (P).")
+            Text("Score is displayed in blue.")
+                .foregroundStyle(.blue)
                 .font(.headline)
+            legendItem(color: .red, text: "Opponent team consists of opponent one (1) and opponent two (2).")
+            Text("Score is displayed in red.")
+                .foregroundStyle(.red)
+                .font(.headline)
+            Text("Server number is displayed in white.")
+                .font(.headline)
+            Text("Highlighted player represents the current server.")
+                .italic()
         }
     }
     
